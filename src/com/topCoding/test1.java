29a28051976a02edd159285af3b1eecf68033372
jaehoonSong <luckysong777@gmail.com>
2018-08-29 18:25:22 +0900

    8/28

--- src/com/topCoding/test1.java
+++ src/com/topCoding/test1.java
@@ -6,36 +6,45 @@ class KiwiJuiceEasy {
 
 	public int[] thePouring(int[] capacities, int[] bottles, int[] fromId,
 			int[] toId) {
+		
+		// 방법 1 ---------------------------------------------------------------
+//		for (int i = 0; i < fromId.length; i++) {
+//
+//			
+//			int f = fromId[i]; // 어디서
+//			int t = toId[i]; // 어디로
+//			int space = capacities[t] - bottles[t]; // 이동 가능한 용량
+//
+//			if (space >= bottles[f]) {
+//				int vol = bottles[f];
+//				bottles[t] += vol;
+//				bottles[f] = 0;
+//			} else {
+//				int vol = space;
+//				bottles[t] += vol;
+//				bottles[f] -= vol;
+//			}
+//
+//		}
+		// ------------------------------------------------------------------------
 
-		for (int i = 0; i < fromId.length; i++) {
-			while (bottles[toId[i]] >= capacities[toId[i]]) {
-				bottles[toId[i]] = bottles[toId[i]]+1;
-				bottles[fromId[i]] = bottles[fromId[i]]-1;
-				if (bottles[fromId[i]] == 0) {
-					break;
-				}
-			}
-
-			// 방법 1
-			// ---------------------------------------------------------------
-			// int f = fromId[i]; // 어디서
-			// int t = toId[i]; // 어디로
-			// int space = capacities[t] - bottles[t]; // 이동 가능한 용량
-			//
-			// if(space >= bottles[f]){
-			// int vol = bottles[f];
-			// bottles[t] += vol;
-			// bottles[f] = 0;
-			// }else{
-			// int vol = space;
-			// bottles[t] += vol;
-			// bottles[f] -= vol;
-			// }
-			//
-			// }
-			// ------------------------------------------------------------------------
-
+		// 방법 1 -----------------------------------------------------------------
+		
+		for(int i = 0 ; i < fromId.length ; i++){
+			
+			int f = fromId[i];
+			int t = toId[i];
+			
+			int sum = bottles[f]+bottles[t];
+			
+			bottles[t] = Math.min(capacities[t], sum);
+			bottles[f] = sum - bottles[t];
+			
 		}
+		
+		
+		
+		// ------------------------------------------------------------------------
 		return bottles;
 	}
 }
